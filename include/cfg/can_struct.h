4125f16254a8e9f994ba1bc3353cc183f1e167bc
Theeramate <theeramate0830@gmail.com>
2024-07-05 17:24:04 +0700

    Split files

---
+++ include/cfg/can_struct.h
@@ -0,0 +1,27 @@
+#ifndef CAN_STRUCT_H
+#define CAN_STRUCT_H
+
+struct CanMessage {
+  uint32_t id;
+  uint8_t data[8];
+  uint32_t period;
+  uint32_t lastSent;
+};
+
+struct CanResponse {
+  uint32_t id;
+  uint8_t data[8];
+  uint32_t responseId;
+  uint8_t responseData[8];
+};
+
+struct CanResponseCheck {
+  uint32_t id;
+  uint8_t data[8];
+};
+
+CanMessage periodicMessages[30];
+CanResponse responseMessages[30];
+CanResponseCheck responseCheck;
+
+#endif 
---
+++ include/cfg/host.h
@@ -0,0 +1,7 @@
+#ifndef HOST_H
+#define HOST_H
+
+#define ssid  "ESP32-CAN-AP"
+#define password  "12345678"
+
+#endif 
