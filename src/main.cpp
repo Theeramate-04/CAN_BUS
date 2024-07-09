457d582ff6cbbe78c8b750243099e9a32a5972e9
Theeramate <theeramate0830@gmail.com>
2024-07-09 09:23:47 +0700

    .

--- src/main.cpp
+++ src/main.cpp
@@ -21,14 +21,16 @@ CanResponseCheck responseCheck;
 void setup() {
   Serial.begin(115200);
   setupAP();
-  server.on("/mode", HTTP_GET, getMode);
-  server.on("/mode", HTTP_POST, setMode);
-  server.on("/period_cfg", HTTP_GET, get_periodic_cfg);
-  server.on("/period_cfg", HTTP_POST, set_periodic_cfg);
-  server.on("/req_res_cfg", HTTP_GET, get_req_res_cfg);
-  server.on("/req_res_cfg", HTTP_POST, set_req_res_cfg);
   server.on("/enable", HTTP_GET, get_program_running);
   server.on("/enable", HTTP_POST, start_stop_program);
+  if (enable == 1) {
+    server.on("/mode", HTTP_GET, getMode);
+    server.on("/mode", HTTP_POST, setMode);
+    server.on("/period_cfg", HTTP_GET, get_periodic_cfg);
+    server.on("/period_cfg", HTTP_POST, set_periodic_cfg);
+    server.on("/req_res_cfg", HTTP_GET, get_req_res_cfg);
+    server.on("/req_res_cfg", HTTP_POST, set_req_res_cfg);
+  }
   server.begin();
 }
 
